73f925aac0444bd74f1abfc6bc5d9352d22cd726
Muhammed Mahmood Alam <muhammed.alam.ext@sivantos.com>
2022-01-02 17:19:17 +0530

    cleaned code

--- src/screens/home/ReleasedMoviesFilter.js
+++ src/screens/home/ReleasedMoviesFilter.js
@@ -1,24 +1,7 @@
 import React from "react";
-
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
 const ReleasedMoviesFilter = (props) => {
-  const [releasedMoviesList, setReleasedMoviesList] = React.useState([]);
-  React.useEffect(() => {
-
-    // fetch(props.baseUrl + "movies?page=1&limit=10", {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "Cache-Control": "no-cache",
-    //   }
-    // })
-    //   .then((response) => response.json())
-    //   .then((response) => {
-    //       setReleasedMoviesList(response.movies)
-    //   })
-    },[])
+  const { setFilter, filter } = props
+  
   return (
     <div>
       <div className="">
