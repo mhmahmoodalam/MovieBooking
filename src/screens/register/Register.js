20a497373243ea0af9166a416fea5dd290c9937e
Muhammed Mahmood Alam <muhammed.alam.ext@sivantos.com>
2022-01-07 19:44:16 +0530

    form validation for register

--- src/screens/register/Register.js
+++ src/screens/register/Register.js
@@ -26,35 +26,76 @@ const Register = (props) => {
         { name :'lastname', label: "Last Name", required: true, type: 'text' },
         { name :'email', label: "Email", required: true, type: 'email' },
         { name :'password', label: "Password", required: true, type: 'password' },
-        { name :'contact', label: "Contact No", required: true, type: 'number' },
+        { name :'contact', label: "Contact No", required: true, type: 'text' },
     ]
     const fieldsErrorDefault = formInputFields.reduce ((prev,field) => {
-        const { name } = field
-        return { ...prev, [name] : false }
-    },{}) 
+      const { name } = field
+      return { ...prev, [name] : { isValid: true, errMsg: 'required' }}
+    },{})  
+
+    const formInitialValues = formInputFields.reduce ((prev,field) => {
+      const { name } = field
+      return { ...prev, [name] : '' }
+    },{})
     const [ formError, setFormError ] = React.useState(fieldsErrorDefault)
 
-    const handleChange = () => {
+    const [ formData, setFormData ] = React.useState(formInitialValues)
 
+    const [ canSubmit, setCanSubmit ] = React.useState(false)
+
+    const handleChange = (e) => {
+      const { name, value } = e.target
+      setFormData({ ...formData, [name] : value })
     }
 
-    const validate = () => {
+    const validate = (values) => {
+      const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      const regexPhone = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
+      const errors = Object.keys(values).reduce((prev,current) =>{
+        const isValid = values[current] !==null && values[current] !=='' ? true : false
+        return { ...prev, [current]: { isValid , errMsg: 'required' } }
+      },{})
 
+      if(errors.email.isValid && !regexEmail.test(values.email)){
+        errors.email = { isValid: false, errMsg: "Invalid email format"}
+      }
+      if(errors.password.isValid && values.password.length < 4 ){
+        errors.password = { isValid: false, errMsg: "Password length less than 4"}
+      }
+      if(errors.contact.isValid && !regexPhone.test(values.contact) ){
+        errors.contact = { isValid: false, errMsg: "Invalid contact no format"}
+      }
+
+      setCanSubmit(Object.keys(errors).reduce((prev,key)=> {
+        return prev && errors[key].isValid
+      },true))
+
+      return errors
     }
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      setFormError(validate(formData))
+      
     }
     
+    React.useEffect(() => {
+
+      if(canSubmit) {
+        // do post request here
+      }
+
+    },[formError])
+
     return (
-      <div className={classes.centerElement}>
+      <div >
+        <form onSubmit={handleSubmit} noValidate autoComplete="off" className={classes.centerElement}>
             {
                 formInputFields.map((field, index) => {
                    return (
-                     <div className={classes.fieldContainer}>
+                     <div className={classes.fieldContainer} key={`formfield_${index}_${field.label}`}>
                        <FormControl
-                         fullWidth
-                         key={`formfield_${index}_${field.label}`}
+                         fullWidth                         
                        >
                          <InputLabel
                            required={field.required}
@@ -65,11 +106,13 @@ const Register = (props) => {
                          <Input
                            type={field.type}
                            id={field.name}
-                           error={formError[field.name]}
+                           name={field.name}
+                           error={!formError[field.name].isValid}
                            onChange={handleChange}
+                           value={formData[field.name]}
                          />
-                         {formError[field.name] && (
-                           <Typography color="error">required</Typography>
+                         {!formError[field.name].isValid && (
+                           <Typography color="error">{formError[field.name].errMsg}</Typography>
                          )}
                        </FormControl>
 
@@ -82,12 +125,12 @@ const Register = (props) => {
           <br />
           <Button
                 variant="contained"
-                onClick={(e) => console.log('form submitted')}
                 color="primary"  
-                             
+                type="submit"             
               >
                 Login
-              </Button>
+          </Button>
+        </form>
       </div>
     );
 }
